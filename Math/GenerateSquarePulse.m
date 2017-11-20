44a0c20322b0c8802734224e3e33863b846915f6
Matthew <mmdstanley@gmail.com>
2017-11-20 14:35:47 -0800

    Created functions to generate three common impulses

---
+++ Math/GenerateSquarePulse.m
@@ -0,0 +1,14 @@
+function [ Function ] = GenerateSquarePulse( Amplitude, StartTime, EndTime )
+%GenerateSquarePusle creates a square pulse
+%   INPUT
+%       Amplitude is the amplitude of the pulse
+%       StartTime is the time when the pulse starts
+%       EndTime is the time when the pulse stops
+%   OUTPUT
+%       Function is the function handle for the response
+    
+    u = @(t) t >= 0;
+    Function = @(t) Amplitude .* (u(t - StartTime) - u(t - EndTime));
+
+end
+
---
+++ Math/GenerateTrianglePulse.m
@@ -0,0 +1,18 @@
+function [ Function ] = GenerateTrianglePulse( Amplitude, StartTime, EndTime )
+%GenerateSquarePusle creates a square pulse
+%   INPUT
+%       Amplitude is the amplitude of the pulse
+%       StartTime is the time when the pulse starts
+%       EndTime is the time when the pulse stops
+%   OUTPUT
+%       Function is the function handle for the response
+    u = @(t) t >= 0;
+    MidTime = (StartTime + EndTime) / 2;
+    
+    Ramp1 = GenerateRampPulse(Amplitude, StartTime, MidTime);
+    Ramp2 = GenerateRampPulse(-Amplitude, MidTime, EndTime);
+    
+    Function = @(t) Ramp1(t) + Ramp2(t) + Amplitude .* (u(t-MidTime) - u(t-EndTime));
+
+end
+
